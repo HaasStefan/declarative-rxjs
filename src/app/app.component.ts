143397f7b943c2bed84d6d17cd32ce06790b4b49
Stefan Haas <stefan.haas.privat@gmail.com>
2022-12-09 23:59:20 +0100

    secret observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, filter, interval, map, of, startWith, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'declarative-rxjs';
+  control = new FormControl<string | null>(null);
+
+  secret$ = this.control.valueChanges.pipe(
+    debounceTime(400),
+    distinctUntilChanged(),
+    filter((value): value is string => !!value && typeof value === 'string'),
+    map((str) => {
+      switch (str) {
+        case 'one':
+          return 1;
+        case 'two':
+          return 2;
+        case 'three':
+          return 3;
+        default:
+          return str;
+      }
+    }),
+    switchMap(value => value === 2 ? interval(100).pipe(startWith(0)) : of(value)),
+    filter((value): value is number => typeof value === 'number'),
+    filter(value => value % 2 === 0),
+    map(value => `Secret number: ${value}`),
+  );
 }
